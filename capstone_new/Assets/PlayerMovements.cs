a64c098ce19e0837f77dcdcf6b7ca7c1af0e2212
TCWCORRUPT <109978819+TCWCORRUPT@users.noreply.github.com>
2023-10-20 09:21:32 +0800

    player movements left and right

--- capstone_new/Assets/PlayerMovements.cs
+++ capstone_new/Assets/PlayerMovements.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class PlayerMovements : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Rigidbody2D rb;
+    //int wholeNumber = 16;
+    //float decimalNumber = 4.56f;
 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey("space"))
+        float dirx = Input.GetAxisRaw("Horizontal");
+        rb.velocity=new Vector2 (dirx*7f,rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") || Input.GetKey("w"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            rb.velocity = new Vector3(rb.velocity.x, 6);
         }
+ 
     }
 }
