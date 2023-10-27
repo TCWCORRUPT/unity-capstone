70e46ce117bd4bcd6bde8f0756790a77bde7f65e
MHWongCayson <J22036962@student.newinti.edu.my>
2023-10-27 17:22:31 +0800

    Redefine character movement
    Added a variable maJumpCount, this allow us to configure the amount of jump the character can make
    
    made a ground and ceiling check, this will check if we are grounded, if grounded, we can jump, eliminating the infinite jump issue

--- capstone_new/Assets/PlayerMovements.cs
+++ capstone_new/Assets/PlayerMovements.cs
@@ -6,26 +6,71 @@ using UnityEngine.EventSystems;
 public class PlayerMovements : MonoBehaviour
 {
     public float movespeed;
+    public float jumpForce;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObject;
+    public float checkRadius;
+    public int maxJumpCount;
+
     private Rigidbody2D rb;
     private bool facingRight = true;
+    private bool isJumping = false;
     private float dirx;
+    private bool isGround;
+    private int jumpCount;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    //private void Start()
-    //{
-        
-    //}
+    private void Start()
+    {
+        jumpCount = maxJumpCount;
+    }
 
     // Update is called once per frame
     private void Update()
     {
-        dirx = Input.GetAxisRaw("Horizontal");
-        rb.velocity=new Vector2 (dirx*7f,rb.velocity.y);
+        inputProcesses();
 
+        animate(); //  flip character
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObject);
+        if(isGround )
+        {
+            jumpCount = maxJumpCount;
+        }
+        movement();
+    }
+
+    private void movement()
+    {
+        rb.velocity = new Vector2(dirx * movespeed, rb.velocity.y);
+        if (isJumping) // Check if the player is on the ground before allowing a jump
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+        }
+        isJumping = false;
+    }
+
+    private void inputProcesses()
+    {
+        dirx = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump")&& jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void animate()
+    {
         if (dirx > 0 && !facingRight)
         {
             flipCharacter();
@@ -34,13 +79,6 @@ public class PlayerMovements : MonoBehaviour
         {
             flipCharacter();
         }
-
-        if (Input.GetButtonDown("Jump") || Input.GetKey("w"))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, 6);
-        }
-
-    
     }
 
     private void flipCharacter()
