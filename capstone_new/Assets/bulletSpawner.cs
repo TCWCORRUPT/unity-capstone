eb97d0ffe43beade87388751b0f14aafe75763af
MHWongCayson <J22036962@student.newinti.edu.my>
2023-12-04 23:29:21 +0800

    Edited level and added gun animation
    level edited, gun animation added

--- capstone_new/Assets/bulletSpawner.cs
+++ capstone_new/Assets/bulletSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class bulletSpawner : MonoBehaviour
 {
     public GameObject bullet;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +18,12 @@ public class bulletSpawner : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.S))
         {
+            animator.SetBool("isShooting", true);
             Instantiate(bullet,transform.position, transform.rotation);
+        }
+        if (!Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("isShooting", false);
         }
     }
 }
