b2d3107423f25be883a6eb84e3f2d3d59620eb2d
j22037289 <152555906+j22037289@users.noreply.github.com>
2023-12-07 05:23:37 +0800

    Fixed portal spawn with mouse
    Mouse0 (left click) spawn entry portal
    Mouse1 (right click) spawn exit portal
    Mouse2 (middle click) despawn all active portals
    fixed portals but need to consider portal entry direction for each as if you want to shoot bullets backwards its hard unless you create a exit portal and shoot thru the exit portal and make the bullets come out the entry portal

--- capstone_new/Assets/portalSpawner.cs
+++ capstone_new/Assets/portalSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class portalSpawner : MonoBehaviour
 {
+    public GameObject entryPortal, exitPortal;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,44 @@ public class portalSpawner : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            GameObject[] checkPortalEntry = GameObject.FindGameObjectsWithTag("EntryPortal");
 
-        }
+			if (checkPortalEntry.Length != 0)
+            {
+                for (int i = 0; i < checkPortalEntry.Length; i++)
+                {
+                    Destroy(checkPortalEntry[i]);
+                }
+            }
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Instantiate(entryPortal, mousePoint, Quaternion.identity);
+        } 
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+			GameObject[] checkPortalExit = GameObject.FindGameObjectsWithTag("ExitPortal");
+
+			if (checkPortalExit.Length != 0)
+			{
+				for (int i = 0; i < checkPortalExit.Length; i++)
+				{
+					Destroy(checkPortalExit[i]);
+				}
+			}
+			Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Instantiate(exitPortal, mousePoint, Quaternion.identity);
+		}
+        else if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+			GameObject[] checkPortalEntry = GameObject.FindGameObjectsWithTag("EntryPortal");
+			GameObject[] checkPortalExit = GameObject.FindGameObjectsWithTag("ExitPortal");
+			for (int i = 0; i < checkPortalEntry.Length; i++)
+			{
+				Destroy(checkPortalEntry[i]);
+			}
+			for (int i = 0; i < checkPortalExit.Length; i++)
+			{
+				Destroy(checkPortalExit[i]);
+			}
+		}
 	}
 }
